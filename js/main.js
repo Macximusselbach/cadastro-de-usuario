b35270394fea57a5e35114a608506d1d68d54005
Macximus <macx.selbach@gmail.com>
2021-02-26 19:28:45 -0300

    Implementando a API Viacep

--- js/main.js
+++ js/main.js
@@ -19,3 +19,19 @@ document.querySelector("#form").addEventListener("submit", (e) => {
 
   console.log(JSON.stringify(user, null, 2));
 });
+
+function preencherEndereco(endereco) {
+  document.querySelector('#logradouro').value = endereco.logradouro;
+  document.querySelector('#bairro').value = endereco.bairro;
+  document.querySelector('#cidade').value = endereco.localidade;
+  document.querySelector('#estado').value = endereco.uf;
+}
+
+document.querySelector("#cep").addEventListener("blur", (e) => {
+  console.log("cep", e.target.value);
+  const cep = e.target.value.replace('-', '');
+
+  fetch(`http://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.json())
+    .then(preencherEndereco);
+});
